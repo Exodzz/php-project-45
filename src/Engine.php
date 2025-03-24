ff4b228374b1455c9cbc1d6dbd5b46d6a6373a91
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-03-24 09:06:17 +0300

    add crap

--- src/Engine.php
+++ src/Engine.php
@@ -8,75 +8,32 @@ use function cli\line;
 use function cli\prompt;
 
 const COUNT_ALLOW = 3;
-/**
- * init
- */
-function playGame(int $answers, array $config): void
+function playGame($config): void
 {
+    $answers = 0;
+    $name = '';
+
     try {
-        $name = '';
-        greetings($name);
-        line($config['answer']);
-        getQuestion($answers, $name, $config);
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+
+        while ($answers < COUNT_ALLOW) {
+            $configQuestion = $config['questionFunc']();
+            $question = $configQuestion['question'];
+            $answer = prompt("Question: " . $question);
+            line("Your answer: $answer");
+
+            $config['validate']($answer, $configQuestion['resultOk']);
+
+            line("Correct!");
+            $answers++;
+
+            if ($answers >= COUNT_ALLOW) {
+                throw new \RuntimeException("Congratulations, $name!");
+            }
+        }
     } catch (\RuntimeException | Error $e) {
         line($e->getMessage());
     }
-}
-
-/**
- * Приветствие
- */
-function greetings(string &$name): void
-{
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-}
-
-/**
- * Запрос вопроса
- */
-function getQuestion(int &$answers, string $name, array $config): bool
-{
-    $configQuestion = $config['questionFunc']();
-    $question = $configQuestion['question'];
-    $answer = prompt("Question: " . $question);
-    line("Your answer: $answer");
-    $config['validate']($answer, $configQuestion['resultOk']);
-    if ($configQuestion['resultOk'] == $answer) {
-        sendSuccess($answers, $name, $config);
-        return true;
-    }
-    sendError($answer, $name, $configQuestion);
-    return true;
-}
-
-/**
- * Отправка сообщения
- */
-function sendError(string $answer, string $name, array $configQuestion): void
-{
-    $message = "'$answer' is wrong answer ;(. Correct answer was '{$configQuestion['resultOk']}'." . PHP_EOL .
-        "Let's try again, $name!";
-    close($message);
-}
-
-function addSuccessAnswer(int &$answers, string $name)
-{
-    $answers++;
-    if ($answers >= COUNT_ALLOW) {
-        close("Congratulations, $name!");
-    }
-}
-
-function sendSuccess(int &$answers, string $name, array $config): void
-{
-    line("Correct!");
-    addSuccessAnswer($answers, $name);
-    getQuestion($answers, $name, $config);
-}
-
-function close(string $message = "Error")
-{
-    throw new \RuntimeException($message);
 }
